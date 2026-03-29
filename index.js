e726598ae48d5e39fdfc619a15ba873e318d047f
aaa-arya <aryasujeet680@gmail.com>
2026-03-29 12:46:22 +0530

    chore: bind server to 0.0.0.0 to allow external network access

--- index.js
+++ index.js
@@ -65,4 +65,4 @@ wss.on('connection', (ws) => {
 });
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+server.listen(PORT, '0.0.0.0', () => console.log(`Server running on port ${PORT}`));
